3aee32ab3ac86fa16aaad2b7faa6d4c54b699391
Arjob Mukherjee <mukherjeearjob@gmail.com>
2022-02-20 23:43:48 +0530

    Match class also stores the related pattern string.
    * Match class stores the corresponding pattern string.

--- src/coderarjob/kpdfsync/lib/pm/Match.java
+++ src/coderarjob/kpdfsync/lib/pm/Match.java
@@ -6,13 +6,15 @@ public class Match
   private int mTotalCount;
   private Line mBeginFrom;
   private Line mEndAt;
+  private String mPattern;
 
-  public Match (int matchCount, int totalCount, Line beginFrom, Line endAt)
+  public Match (int matchCount, int totalCount, Line beginFrom, Line endAt, String pattern)
   {
     this.mMatchCount = matchCount;
     this.mTotalCount = totalCount;
     this.mBeginFrom = beginFrom;
     this.mEndAt = endAt;
+    this.mPattern = pattern;
   }
 
   public float matchPercent () { return (float)this.mMatchCount/this.mTotalCount * 100; }
@@ -20,5 +22,6 @@ public class Match
   public int totalCount () { return this.mTotalCount; }
   public Line beginFrom () { return this.mBeginFrom; }
   public Line endAt () { return this.mEndAt; }
+  public String pattern () { return this.mPattern; }
 
 }
