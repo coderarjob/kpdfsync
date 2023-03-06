0c6ad16754dc739b57da898dca4010fdbce31de4
Arjob Mukherjee <mukherjeearjob@gmail.com>
2023-03-07 01:55:12 +0530

    Cross platform version string for each package.
    Fixes previous drawbacks of version string creation.
    
    Previous Drawbacks:
    > Only works in Linux as of now.
    Now works on Windows as well.
    
    > GIT absence is not handled.
    Git is checked and handled is not found - COMMIT_ID is `Unknown` is Git
    was not found.
    
    BUILD_ID depends on `date` in Linux and `powershell` in Windows, if they
    are not found BUILD_ID is set to `Unknown`.

--- cmake/GetVersion.cmake
+++ cmake/GetVersion.cmake
@@ -1,4 +1,6 @@
+
 function(ConfigureVersionInFile target pre_file post_file)
+
     add_custom_target(${target} ALL
         DEPENDS ${pre_file}
         BYPRODUCTS ${post_file}
@@ -10,16 +12,50 @@ function(ConfigureVersionInFile target pre_file post_file)
                 -P ${CMAKE_SOURCE_DIR}/cmake/GetVersion.cmake)
 endfunction()
 
-function(CreateVersion)
-    execute_process(
-        COMMAND git log --format=%h -n 1 ${source_dir}
-        OUTPUT_STRIP_TRAILING_WHITESPACE
-        OUTPUT_VARIABLE COMMIT_ID)
+function(GetCommitID var)
+    find_package(Git)
 
-    execute_process(
-        COMMAND date +%y%m%d
-        OUTPUT_STRIP_TRAILING_WHITESPACE
-        OUTPUT_VARIABLE BUILD_ID)
+    if (Git_FOUND)
+        execute_process(
+            COMMAND git log --format=%h -n 1 ${source_dir}
+            OUTPUT_STRIP_TRAILING_WHITESPACE
+            OUTPUT_VARIABLE commit_id)
+
+        set(${var} ${commit_id} PARENT_SCOPE)
+    endif()
+
+endfunction()
+
+function(GetBuildID var)
+    if(UNIX)
+        find_program(DATE date)
+        if (DATE)
+            set(date_command date +%y%m%d)
+        endif()
+    elseif(WIN32)
+        find_program(POWERSHELL powershell)
+        if (POWERSHELL)
+            set(date_command powershell -Command Get-Date -Format "yymmdd")
+        endif()
+    endif()
+
+    if (DATE OR POWERSHELL)
+        execute_process(
+            COMMAND ${date_command}
+            OUTPUT_STRIP_TRAILING_WHITESPACE
+            OUTPUT_VARIABLE build_id)
+
+        set(${var} ${build_id} PARENT_SCOPE)
+    endif()
+
+endfunction()
+
+function(CreateVersion)
+    set(COMMIT_ID Unknown)
+    set(BUILD_ID Unknown)
+
+    GetCommitID(COMMIT_ID)
+    GetBuildID(BUILD_ID)
 
     configure_file(${infile} ${outfile})
 endfunction()
